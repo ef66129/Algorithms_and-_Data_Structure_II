4a6e309f91d0f9a4b822d76beb63993fc42b0418
EF66_129 <ef66129@users.noreply.github.com>
2015-10-03 11:13:50 +0900

    First commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+某アルゴリズムとデータ構造の解答例です。
+標準C99です。
+
+【MSVC++2010での注意】
+MSVC++2010は標準C99に対応していないのでC++としてコンパイルしないと動作しません。
+また、UTF-8のソースコードのコンパイルにも対応していないので自分でCP932に直してからコンパイルし直してください。
+
+【ライセンス】
+!! ライセンスは必ず厳守してください !!
+
+Copyright (c) 2015 EF66_129
+Released under the MIT license
+http://opensource.org/licenses/mit-license.php
---
+++ kadai1.c
@@ -0,0 +1,31 @@
+/*
+Copyright (c) 2015 EF66_129
+Released under the MIT license
+http://opensource.org/licenses/mit-license.php
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int data[] = {17, 39, 1, 9, 5, 24, 2, 11, 23, 6};
+	int i, sum = 0;
+	double avg, var = 0.0;
+
+	for (i = 0; i < 10; i++) {
+		sum += data[i];
+	}
+	avg = sum / 10.0;
+
+	for (i = 0; i < 10; i++) {
+		var += pow(avg - data[i], 2.0);
+	}
+	var /= 9.0;
+
+
+	printf("•½‹Ï: %f\n", avg);
+	printf("•ªŽU: %f\n", var);
+
+	return 0;
+}
